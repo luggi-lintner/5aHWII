877354f85b89571ffcc1c8faa759b46d4f930f9f
luggi-lintner <lulintner@tsn.at>
2019-10-02 14:46:43 +0200

    First Commit

---
+++ Logger/Loger/src/src/Main.java
@@ -0,0 +1,24 @@
+package src;
+import src.Singleton;
+import java.io.*;
+
+public class Main {
+	
+	
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Singleton.getInstance();
+		Singleton.log("jgdhogsfjsdkgnjdalgndsgsdgsdojg");
+	
+
+	}
+	
+	
+	
+	
+	//log("Programm initialsiert");
+	//Singleton.getInstance().log("Person erzeugt");
+}
+
